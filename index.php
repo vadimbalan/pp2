26ae814471f3bd3619459cce3daf7240d84cd6a9
Vadim Balan <vbalan@mail.greenriver.edu>
2020-04-20 20:25:53 -0400

    Added index.php file

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Name: Vadim Balan
+ * Date: 4/20/2020
+ * C:\Users\vvadi\PhpstormProjects\GreenRiverDev\328\pp2\index.php
+ * Pair Program 2
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+
+<?php
+echo "<p>PHP Array Practice</p>";
+?>
+
+</body>
+</html>
